aa2f91aaceaaa91e4ee883927f659d350b17208c
Krzysztof Mazur <krz@ychu.pl>
2017-02-18 22:52:43 +0100

    Update bundle DI configuration

--- src/KrzysztofMazur/ObjectMapperBundle/DependencyInjection/ObjectMapperExtension.php
+++ src/KrzysztofMazur/ObjectMapperBundle/DependencyInjection/ObjectMapperExtension.php
@@ -11,6 +11,7 @@ namespace KrzysztofMazur\ObjectMapperBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 
 /**
