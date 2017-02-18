261ba29579f3c20db0ea163e390e49d8e759851d
Krzysztof Mazur <krz@ychu.pl>
2017-02-18 19:42:55 +0100

    Partial container configuration

---
+++ src/KrzysztofMazur/ObjectMapperBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * This file is part of php-object-mapper.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KrzysztofMazur\ObjectMapperBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * @author Krzysztof Mazur <krz@ychu.pl>
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $tb = new TreeBuilder();
+
+        $tb->root('km_object_mapper', 'array')
+            ->children()
+                ->scalarNode('instantiator_id')->defaultValue("km_object_mapper.instantiator.doctrine_adapter")->end()
+                ->arrayNode('configuration_locations')->cannotBeEmpty()->end()
+            ->end();
+
+        return $tb;
+    }
+}
---
+++ src/KrzysztofMazur/ObjectMapperBundle/ObjectMapperBundle.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file is part of php-object-mapper.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KrzysztofMazur\ObjectMapperBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Krzysztof Mazur <krz@ychu.pl>
+ */
+class ObjectMapperBundle extends Bundle
+{
+
+}
